c02d4623738d5d56b1ae1447e230b834cb651cf9
saki30 <s6u6s024@gmail.com>
2025-04-16 12:54:59 +0900

    ログイン画面修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,14 +38,14 @@
             </a>
 
             @if (Route::has('password.request'))
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ml-3" href="{{ route('password.request') }}>
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ml-3" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
 
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
-                </x-primary-button>
+            </x-primary-button>
         </div>
     </form>
 </x-guest-layout>
